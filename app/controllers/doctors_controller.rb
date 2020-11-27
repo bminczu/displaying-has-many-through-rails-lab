7bb9cbd1056db794788fddb83bb8b0465db27d75
bminczu <bminczu@gmail.com>
2020-11-27 13:56:55 -0600

    commit tree c30a339d66d338bf9c848c956279515810cf1b80

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,25 @@
+class DoctorsController < ApplicationController
+
+    def index
+        @doctors = Doctor.all
+    end
+
+    def new
+    end
+
+    def show
+    end
+
+    def create 
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
